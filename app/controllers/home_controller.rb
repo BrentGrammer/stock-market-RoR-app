2939d07cceefe018118979fb34c2726747e85dce
BrentGrammer <brentmarquez@gmail.com>
2019-02-24 20:23:53 +0000

    add devise gem for auth

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,20 @@
 class HomeController < ApplicationController
   def index
+    
+    if params[:id] == ""
+      @nothing = "Please enter a symbol."
+    elsif
+      if params[:id]
+        begin
+          @stock = StockQuote::Stock.quote(params[:id])
+        rescue 
+          @error = "Does not exist"
+        end
+      end
+    end
+    
   end
+
   
   def about
   end
